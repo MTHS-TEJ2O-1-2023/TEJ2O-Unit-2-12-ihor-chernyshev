afafa6e0f1064d7a71788ac5005ae49d5416c56e
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-31 21:17:04 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,6 +4,10 @@ Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
+# variables
+dist_cm = 0
+
+# setup
 from microbit import *
 import neopixel
 from machine import time_pulse_us
@@ -23,6 +27,7 @@ echo = pin2
 tring.write_digital(1)
 echo.read_digital()
 
+# find the distance
 while True:
     if button_a.was_pressed():
         tring.write_digital(1)
@@ -30,23 +35,25 @@ while True:
         micros = time_pulse_us(echo, 2)
         t_echo = micros / 1000000
         dist_cm = (t_echo / 2) * 34300
-    if dist_cm < 10:
-        np[0] = (255, 0, 0)
-        np[1] = (255, 0, 0)
-        np[2] = (255, 0, 0)
-        np[3] = (255, 0, 0)
-        print(np[0], np[1], np[2], np[3])
-        np.show()
-        sleep(1000)
-        np.clear()
-        display.show(Image.HAPPY)
-    elif dist_cm >= 10:
-        np[0] = (0, 0, 0)
-        np[1] = (0, 0, 0)
-        np[2] = (0, 0, 0)
-        np[3] = (0, 0, 0)
-        print(np[0], np[1], np[2], np[3])
-        np.show()
-        sleep(1000)
-        np.clear()
-        display.show(Image.HAPPY)
+        if dist_cm < 10:
+            display.scroll(str(int(dist_cm)))
+            np[0] = (255, 0, 0)
+            np[1] = (255, 0, 0)
+            np[2] = (255, 0, 0)
+            np[3] = (255, 0, 0)
+            print(np[0], np[1], np[2], np[3])
+            np.show()
+            display.show(Image.HAPPY)
+            sleep(1000)
+            np.clear()
+        elif dist_cm >= 10:
+            display.scroll(str(int(dist_cm)))
+            np[0] = (0, 255, 0)
+            np[1] = (0, 255, 0)
+            np[2] = (0, 255, 0)
+            np[3] = (0, 255, 0)
+            print(np[0], np[1], np[2], np[3])
+            np.show()
+            display.show(Image.HAPPY)
+            sleep(1000)
+            np.clear()
